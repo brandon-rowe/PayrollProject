3e8e67975091b7dd8942b3151f7e2465b54da268
alonsoi <aalonsoisaac.nsfw@gmail.com>
2019-09-18 13:20:21 -0400

    update forms

---
+++ PayrollGUI_Proto/PayrollGUI_Proto/CompanyInformationForm.cpp
@@ -0,0 +1,2 @@
+#include "CompanyInformationForm.h"
+
---
+++ PayrollGUI_Proto/PayrollGUI_Proto/PayrollProto.cpp
@@ -0,0 +1,11 @@
+#include "PayrollProto.h"
+#include "LoginAndCreateUserForm.h"
+#include "CompanyInformationForm.h"
+#include "AddEmployeeForm.h"
+#include <Windows.h>
+
+int main()
+{
+	return 0;
+}
+
