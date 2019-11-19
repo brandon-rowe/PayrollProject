69fc3d32c3c4a352d75859cafcb06ed3e4c467b2
Madhav Kumar <madhavrkumar@gmail.com>
2019-11-18 23:23:13 -0500

    found small flaw in login validation logic using unit tests, fixed it

--- Payroll_ProtoVB/Payroll.Test/LoginTest.vb
+++ Payroll_ProtoVB/Payroll.Test/LoginTest.vb
@@ -3,14 +3,22 @@ Imports Payroll_ProtoVB
 <TestFixture>
 Public Class LoginTest
 
-    <Test>
-    Sub ValidationCheck()
+    <TestCase("First", "Last", "123 Street", "SC", "12345", "password", "password", ExpectedResult:=True)>
+    <TestCase("First", "Last", "123 Street", "SC", "12345", "password", "password1", ExpectedResult:=False)>
+    <TestCase("", "Last", "123 Street", "SC", "12345", "password", "password", ExpectedResult:=False)>
+    <TestCase("First", "", "123 Street", "SC", "12345", "password", "password", ExpectedResult:=False)>
+    <TestCase("First", "Last", "", "SC", "12345", "password", "password", ExpectedResult:=False)>
+    <TestCase("First", "Last", "123 Street", "", "12345", "password", "password", ExpectedResult:=False)>
+    <TestCase("First", "Last", "123 Street", "SC", "", "password", "password", ExpectedResult:=False)>
+    <TestCase("First", "Last", "123 Street", "SC", "12345", "", "password", ExpectedResult:=False)>
+    <TestCase("First", "Last", "123 Street", "SC", "12345", "password", "", ExpectedResult:=False)>
+    <TestCase("First", "Last", "123 Street", "SC", "12345", "", "", ExpectedResult:=False)>
+    Function ValidationCheck(FName As String, LName As String, Street As String, State As String, Zip As String, PWord As String, VPWord As String) As Boolean
         Dim LgIn As LoginLogic = New LoginLogic()
         Dim result As Boolean
 
-        result = LgIn.IsCreateUsrFormValid("First", "Last", "123 Street", "SC", "12345", "password", "password")
-        Assert.True(result)
-
-    End Sub
+        result = LgIn.IsCreateUsrFormValid(FName, LName, Street, State, Zip, PWord, VPWord)
+        Return result
+    End Function
 
 End Class
--- Payroll_ProtoVB/Payroll_ProtoVB/LoginLogic.vb
+++ Payroll_ProtoVB/Payroll_ProtoVB/LoginLogic.vb
@@ -3,7 +3,7 @@
 
     Public Function IsCreateUsrFormValid(FName As String, LName As String, Street As String, State As String, Zip As String, PWord As String, VPWord As String) As Boolean
 
-        Return FName.Length() > 0 AndAlso LName.Length() > 0 AndAlso Street.Length() > 0 AndAlso State.Length() > 0 AndAlso Zip.Length() > 0 AndAlso PWord = VPWord
+        Return FName.Length() > 0 AndAlso LName.Length() > 0 AndAlso Street.Length() > 0 AndAlso State.Length() > 0 AndAlso Zip.Length() > 0 AndAlso PWord = VPWord AndAlso PWord.Length() > 0
 
 
     End Function
